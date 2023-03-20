b86f30009ce76a51ea9ff494dedc8176f7f3b5b3
amitjait <amit.rimit591@gmail.com>
2023-03-20 23:28:57 +0530

    Adding

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,13 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+{/* <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p> */}
 
+import React from "react";
+
+function App(){
+    return (
+        <div>
+            <h1>I can make React app now!</h1>
+        </div>
+    )
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
+import { render } from "enzyme";
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 
 
